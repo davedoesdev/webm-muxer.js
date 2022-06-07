97a2c52442dcc8b4a8ee3eb5e98351f35d6d3a9f
David Halls <dahalls@gmail.com>
2022-06-07 21:57:51 +0100

    Fix passing of private data

--- demo.js
+++ demo.js
@@ -14,6 +14,7 @@ const record_el = document.getElementById('record');
 const pcm_el = document.getElementById('pcm');
 const inmem_el = document.getElementById('in-memory');
 const codec_el = document.getElementById('codec');
+const chroma_el = document.getElementById('chroma');
 let video_track, audio_track;
 
 const video = document.getElementById('video');
@@ -38,35 +39,35 @@ record_el.addEventListener('input', function () {
 pcm_el.disabled = true;
 inmem_el.disabled = true;
 
-// See https://www.webmproject.org/vp9/mp4/
-// and also https://googlechrome.github.io/samples/media/vp9-codec-string.html
-const vp9_params = {
-    profile: 0,
-    level: 10,
-    bit_depth: 8,
-    chroma_subsampling: 1
-};
-const vp9c = Object.fromEntries(Object.entries(vp9_params).map(
-    ([k, v]) => [k, v.toString().padStart(2, '0')]));
-const vp9_codec = `vp09.${vp9c.profile}.${vp9c.level}.${vp9c.bit_depth}.${vp9c.chroma_subsampling}`;
-
-// See https://github.com/ietf-wg-cellar/matroska-specification/blob/master/codec/av1.md
-// and also https://aomediacodec.github.io/av1-isobmff/#codecsparam
-const av1_params = {
-    profile: 0,
-    level: 0,
-    tier: 0,
-    high_bitdepth: false,
-    twelve_bit: false,
-    monochrome: false,
-    chroma_subsampling_x: false,
-    chroma_subsampling_y: false,
-    chroma_sample_position: 0,
-};
-const av1_bitdepth = 8 + av1_params.high_bitdepth * (av1_params.profile === 2 && av1_params.twelve_bit ? 4 : 2)
-const av1_codec = `av01.${av1_params.profile}.${av1_params.level.toString().padStart(2, '0')}${av1_params.tier === 0 ? 'M' : 'H'}.${av1_bitdepth.toString().padStart(2, '0')}`;//.${av1_params.chroma_subsampling_x+0}${av1_params.chroma_subsampling_y+0}${av1_params.chroma_sample_position}`;
-
 start_el.addEventListener('click', async function () {
+    // See https://www.webmproject.org/vp9/mp4/
+    // and also https://googlechrome.github.io/samples/media/vp9-codec-string.html
+    const vp9_params = {
+        profile: 0,
+        level: 10,
+        bit_depth: 8,
+        chroma_subsampling: chroma.value ? 2 : 1
+    };
+    const vp9c = Object.fromEntries(Object.entries(vp9_params).map(
+        ([k, v]) => [k, v.toString().padStart(2, '0')]));
+    const vp9_codec = `vp09.${vp9c.profile}.${vp9c.level}.${vp9c.bit_depth}.${vp9c.chroma_subsampling}`;
+
+    // See https://github.com/ietf-wg-cellar/matroska-specification/blob/master/codec/av1.md
+    // and also https://aomediacodec.github.io/av1-isobmff/#codecsparam
+    const av1_params = {
+        profile: 0,
+        level: 0,
+        tier: 0,
+        high_bitdepth: false,
+        twelve_bit: false,
+        monochrome: false,
+        chroma_subsampling_x: !!chroma.value,
+        chroma_subsampling_y: !!chroma.value,
+        chroma_sample_position: 0,
+    };
+    const av1_bitdepth = 8 + av1_params.high_bitdepth * (av1_params.profile === 2 && av1_params.twelve_bit ? 4 : 2)
+    const av1_codec = `av01.${av1_params.profile}.${av1_params.level.toString().padStart(2, '0')}${av1_params.tier === 0 ? 'M' : 'H'}.${av1_bitdepth.toString().padStart(2, '0')}`;//.${av1_params.chroma_subsampling_x+0}${av1_params.chroma_subsampling_y+0}${av1_params.chroma_sample_position}`;
+
     this.disabled = true;
     record_el.disabled = true;
     pcm_el.disabled = true;
--- tests/tests.spec.mjs
+++ tests/tests.spec.mjs
@@ -77,169 +77,176 @@ function polyfillShowSaveFilePicker() {
 
 for (let codec of ['vp09', 'av01']) {
     for (let pcm of [false, true]) {
-        test(`records to a valid WebM file (codec=${codec},pcm=${pcm})`, async ({ page }) => {
-            await page.goto('demo.html');
-            expect(await page.title()).toBe('WebM muxer demo');
-            await page.check('#record');
-            if (pcm) {
-                await page.click('#pcm');
-            }
-            await page.selectOption('#codec', codec);
-            await page.evaluate(polyfillShowSaveFilePicker);
-            await page.click('#start');
-            let width, height;
-            if (pcm) {
-                await setTimeoutP(12000);
-            } else {
-                await page.waitForFunction(() => !document.getElementById('video').paused);
-                await page.waitForFunction(() => document.getElementById('video').currentTime > 10);
-                width = await page.evaluate(() => document.getElementById('video').videoWidth);
-                height = await page.evaluate(() => document.getElementById('video').videoHeight);
-                expect(width).toBeGreaterThan(0);
-                expect(height).toBeGreaterThan(0);
-            }
+        for (let chroma of [false, true]) {
+            test(`records to a valid WebM file (codec=${codec},pcm=${pcm},chroma=${chroma})`, async ({ page }) => {
+                await page.goto('demo.html');
+                expect(await page.title()).toBe('WebM muxer demo');
+                await page.check('#record');
+                if (pcm) {
+                    await page.click('#pcm');
+                }
+                await page.selectOption('#codec', codec);
+                if (chroma) {
+                    await page.locator('#chroma').evaluate(node => node.value = 'y');
+                }
+                await page.evaluate(polyfillShowSaveFilePicker);
+                await page.click('#start');
+                let width, height;
+                if (pcm) {
+                    await setTimeoutP(12000);
+                } else {
+                    await page.waitForFunction(() => !document.getElementById('video').paused);
+                    await page.waitForFunction(() => document.getElementById('video').currentTime > 10);
+                    width = await page.evaluate(() => document.getElementById('video').videoWidth);
+                    height = await page.evaluate(() => document.getElementById('video').videoHeight);
+                    expect(width).toBeGreaterThan(0);
+                    expect(height).toBeGreaterThan(0);
+                }
 
-            const [ download ] = await Promise.all([
-                page.waitForEvent('download'),
-                page.click('#stop')
-            ]);
-            const path = await download.path();
-            let { stdout, stderr } = await execFileP('mediainfo', [ '--Output=JSON', path ]);
-            log_on_fail += stdout;
-            expect(stderr).toBe('');
-            const tracks = {};
-            for (let track of JSON.parse(stdout).media.track) {
-                tracks[track['@type']] = track;
-            }
-            expect(tracks.General.VideoCount).toBe('1');
-            expect(tracks.General.AudioCount).toBe('1');
-            expect(tracks.General.Format).toBe(pcm ? 'Matroska' : 'WebM');
-            expect(tracks.General.Format_Version).toBe('4');
-            expect(parseFloat(tracks.General.Duration)).toBeGreaterThanOrEqual(10);
-            expect(tracks.General.IsStreamable).toBe('Yes');
-            expect(tracks.General.Encoded_Application).toBe('WebMLiveMuxer');
-            expect(tracks.General.Encoded_Library).toBe('libwebm-0.3.0.0');
+                const [ download ] = await Promise.all([
+                    page.waitForEvent('download'),
+                    page.click('#stop')
+                ]);
+                const path = await download.path();
+                let { stdout, stderr } = await execFileP('mediainfo', [ '--Output=JSON', path ]);
+                log_on_fail += stdout;
+                expect(stderr).toBe('');
+                const tracks = {};
+                for (let track of JSON.parse(stdout).media.track) {
+                    tracks[track['@type']] = track;
+                }
+                expect(tracks.General.VideoCount).toBe('1');
+                expect(tracks.General.AudioCount).toBe('1');
+                expect(tracks.General.Format).toBe(pcm ? 'Matroska' : 'WebM');
+                expect(tracks.General.Format_Version).toBe('4');
+                expect(parseFloat(tracks.General.Duration)).toBeGreaterThanOrEqual(10);
+                expect(tracks.General.IsStreamable).toBe('Yes');
+                expect(tracks.General.Encoded_Application).toBe('WebMLiveMuxer');
+                expect(tracks.General.Encoded_Library).toBe('libwebm-0.3.0.0');
 
-            expect(tracks.Video.Format).toBe(codec === 'av01' ? 'AV1' : 'VP9');
-            expect(tracks.Video.CodecID).toBe(codec === 'av01' ? 'V_AV1' : 'V_VP9');
-            if (tracks.Video.FrameRate_Mode === 'VFR') {
-                // mediainfo does VFR detection using timecodes so occasionally we trigger that detection
-                // https://github.com/MediaArea/MediaInfoLib/commit/f935443d48731c6524a69e8c25a04fcde9b4547d
-                expect(parseFloat(tracks.Video.FrameRate_Original)).toBe(30);
-            } else {
-                expect(tracks.Video.FrameRate_Mode).toBe('CFR');
-                expect(parseFloat(tracks.Video.Duration)).toBeGreaterThanOrEqual(10);
-            }
-            if (pcm) {
-                width = parseInt(tracks.Video.Width);
-                height = parseInt(tracks.Video.Height);
-            } else {
-                expect(parseInt(tracks.Video.Width)).toBe(width);
-                expect(parseInt(tracks.Video.Height)).toBe(height);
-            }
+                expect(tracks.Video.Format).toBe(codec === 'av01' ? 'AV1' : 'VP9');
+                expect(tracks.Video.CodecID).toBe(codec === 'av01' ? 'V_AV1' : 'V_VP9');
+                if (tracks.Video.FrameRate_Mode === 'VFR') {
+                    // mediainfo does VFR detection using timecodes so occasionally we trigger that detection
+                    // https://github.com/MediaArea/MediaInfoLib/commit/f935443d48731c6524a69e8c25a04fcde9b4547d
+                    expect(parseFloat(tracks.Video.FrameRate_Original)).toBe(30);
+                } else {
+                    expect(tracks.Video.FrameRate_Mode).toBe('CFR');
+                    expect(parseFloat(tracks.Video.Duration)).toBeGreaterThanOrEqual(10);
+                }
+                if (pcm) {
+                    width = parseInt(tracks.Video.Width);
+                    height = parseInt(tracks.Video.Height);
+                } else {
+                    expect(parseInt(tracks.Video.Width)).toBe(width);
+                    expect(parseInt(tracks.Video.Height)).toBe(height);
+                }
 
-            expect(tracks.Audio.Format).toBe(pcm ? 'PCM' : 'Opus');
-            expect(tracks.Audio.CodecID).toBe(pcm ? 'A_PCM/FLOAT/IEEE' : 'A_OPUS');
-            expect(parseFloat(tracks.Audio.Duration)).toBeGreaterThanOrEqual(10);
-            expect(tracks.Audio.Channels).toBe('1');
-            if (!pcm) {
-                expect(tracks.Audio.ChannelPositions).toBe('Front: C');
-                expect(tracks.Audio.ChannelLayout).toBe('C');
-                expect(tracks.Audio.Compression_Mode).toBe('Lossy');
-            }
-            expect(tracks.Audio.SamplingRate).toBe('48000');
-            expect(parseFloat(tracks.Audio.Delay)).toBe(0);
+                expect(tracks.Audio.Format).toBe(pcm ? 'PCM' : 'Opus');
+                expect(tracks.Audio.CodecID).toBe(pcm ? 'A_PCM/FLOAT/IEEE' : 'A_OPUS');
+                expect(parseFloat(tracks.Audio.Duration)).toBeGreaterThanOrEqual(10);
+                expect(tracks.Audio.Channels).toBe('1');
+                if (!pcm) {
+                    expect(tracks.Audio.ChannelPositions).toBe('Front: C');
+                    expect(tracks.Audio.ChannelLayout).toBe('C');
+                    expect(tracks.Audio.Compression_Mode).toBe('Lossy');
+                }
+                expect(tracks.Audio.SamplingRate).toBe('48000');
+                expect(parseFloat(tracks.Audio.Delay)).toBe(0);
 
-            ({ stdout, stderr } = await execFileP('mkvmerge', [ '-J', path ]));
-            log_on_fail += stdout;
-            expect(stderr).toBe('');
-            const info = JSON.parse(stdout);
+                ({ stdout, stderr } = await execFileP('mkvmerge', [ '-J', path ]));
+                log_on_fail += stdout;
+                expect(stderr).toBe('');
+                const info = JSON.parse(stdout);
 
-            expect(info.identification_format_version).toBeGreaterThanOrEqual(12);
-            expect(info.attachments.length).toBe(0);
-            expect(info.chapters.length).toBe(0);
-            expect(info.container.properties.container_type).toBe(17); // https://gitlab.com/mbunkus/mkvtoolnix/-/blob/main/src/common/file_types.h file_type_e::matroska
-            expect(info.container.properties.duration).toBeGreaterThanOrEqual(10000000000);
-            expect(info.container.properties.is_providing_timestamps).toBe(true);
-            expect(info.container.properties.muxing_application).toBe('libwebm-0.3.0.0');
-            expect(info.container.properties.writing_application).toBe('WebMLiveMuxer');
-            expect(info.container.recognized).toBe(true);
-            expect(info.container.supported).toBe(true);
-            expect(info.container.type).toBe('Matroska');
-            expect(info.errors.length).toBe(0);
-            expect(info.warnings.length).toBe(0);
-            expect(info.file_name).toBe(path);
-            expect(info.global_tags.length).toBe(0);
-            expect(info.track_tags.length).toBe(0);
-            expect(info.tracks.length).toBe(2);
+                expect(info.identification_format_version).toBeGreaterThanOrEqual(12);
+                expect(info.attachments.length).toBe(0);
+                expect(info.chapters.length).toBe(0);
+                expect(info.container.properties.container_type).toBe(17); // https://gitlab.com/mbunkus/mkvtoolnix/-/blob/main/src/common/file_types.h file_type_e::matroska
+                expect(info.container.properties.duration).toBeGreaterThanOrEqual(10000000000);
+                expect(info.container.properties.is_providing_timestamps).toBe(true);
+                expect(info.container.properties.muxing_application).toBe('libwebm-0.3.0.0');
+                expect(info.container.properties.writing_application).toBe('WebMLiveMuxer');
+                expect(info.container.recognized).toBe(true);
+                expect(info.container.supported).toBe(true);
+                expect(info.container.type).toBe('Matroska');
+                expect(info.errors.length).toBe(0);
+                expect(info.warnings.length).toBe(0);
+                expect(info.file_name).toBe(path);
+                expect(info.global_tags.length).toBe(0);
+                expect(info.track_tags.length).toBe(0);
+                expect(info.tracks.length).toBe(2);
 
-            expect(info.tracks[0].codec).toBe(codec === 'av01' ? 'AV1' : 'VP9');
-            expect(info.tracks[0].id).toBe(0);
-            expect(info.tracks[0].type).toBe('video');
-            expect(info.tracks[0].properties.codec_id).toBe(codec === 'av01' ? 'V_AV1' : 'V_VP9');
-            expect(info.tracks[0].properties.codec_private_data).toBe(codec === 'av01' ? '81000000' : '01010002010a030108040101');
-            expect(info.tracks[0].properties.codec_private_length).toBe(codec === 'av01' ? 4 : 12);
-            if (info.identification_format_version >= 14) {
-                expect(info.tracks[0].properties.default_duration).toBe(Math.floor(1000000000 / 30)); // (1s / frame rate)
-            }
-            expect(info.tracks[0].properties.default_track).toBe(true);
-            expect(info.tracks[0].properties.display_dimensions).toBe(`${width}x${height}`);
-            expect(info.tracks[0].properties.display_unit).toBe(0); // pixels
-            expect(info.tracks[0].properties.enabled_track).toBe(true);
-            expect(info.tracks[0].properties.forced_track).toBe(false);
-            expect(info.tracks[0].properties.language).toBe('eng');
-            expect(info.tracks[0].properties.minimum_timestamp).toBe(0);
-            expect(info.tracks[0].properties.number).toBe(1);
-            expect(info.tracks[0].properties.pixel_dimensions).toBe(`${width}x${height}`);
+                expect(info.tracks[0].codec).toBe(codec === 'av01' ? 'AV1' : 'VP9');
+                expect(info.tracks[0].id).toBe(0);
+                expect(info.tracks[0].type).toBe('video');
+                expect(info.tracks[0].properties.codec_id).toBe(codec === 'av01' ? 'V_AV1' : 'V_VP9');
+                expect(info.tracks[0].properties.codec_private_data).toBe(
+                    codec === 'av01' ? ('81000' + (chroma ? 'c' : '0') + '00') :
+                                       ('01010002010a03010804010' + (chroma ? '2' : '1')));
+                expect(info.tracks[0].properties.codec_private_length).toBe(codec === 'av01' ? 4 : 12);
+                if (info.identification_format_version >= 14) {
+                    expect(info.tracks[0].properties.default_duration).toBe(Math.floor(1000000000 / 30)); // (1s / frame rate)
+                }
+                expect(info.tracks[0].properties.default_track).toBe(true);
+                expect(info.tracks[0].properties.display_dimensions).toBe(`${width}x${height}`);
+                expect(info.tracks[0].properties.display_unit).toBe(0); // pixels
+                expect(info.tracks[0].properties.enabled_track).toBe(true);
+                expect(info.tracks[0].properties.forced_track).toBe(false);
+                expect(info.tracks[0].properties.language).toBe('eng');
+                expect(info.tracks[0].properties.minimum_timestamp).toBe(0);
+                expect(info.tracks[0].properties.number).toBe(1);
+                expect(info.tracks[0].properties.pixel_dimensions).toBe(`${width}x${height}`);
 
-            expect(info.tracks[1].codec).toBe(pcm ? 'PCM' : 'Opus');
-            expect(info.tracks[1].id).toBe(1);
-            expect(info.tracks[1].type).toBe('audio');
-            expect(info.tracks[1].properties.audio_channels).toBe(1);
-            expect(info.tracks[1].properties.audio_sampling_frequency).toBe(48000);
-            expect(info.tracks[1].properties.codec_id).toBe(pcm ? 'A_PCM/FLOAT/IEEE' : 'A_OPUS');
-            if (!pcm) {
-                expect(info.tracks[1].properties.codec_private_data).toBe('4f707573486561640101000080bb0000000000');
-            }
-            expect(info.tracks[1].properties.codec_private_length).toBe(pcm ? 0 : 19);
-            expect(info.tracks[1].properties.default_track).toBe(true);
-            expect(info.tracks[1].properties.enabled_track).toBe(true);
-            expect(info.tracks[1].properties.forced_track).toBe(false);
-            expect(info.tracks[1].properties.language).toBe('eng');
-            expect(info.tracks[1].properties.minimum_timestamp).toBe(0);
-            expect(info.tracks[1].properties.number).toBe(2);
+                expect(info.tracks[1].codec).toBe(pcm ? 'PCM' : 'Opus');
+                expect(info.tracks[1].id).toBe(1);
+                expect(info.tracks[1].type).toBe('audio');
+                expect(info.tracks[1].properties.audio_channels).toBe(1);
+                expect(info.tracks[1].properties.audio_sampling_frequency).toBe(48000);
+                expect(info.tracks[1].properties.codec_id).toBe(pcm ? 'A_PCM/FLOAT/IEEE' : 'A_OPUS');
+                if (!pcm) {
+                    expect(info.tracks[1].properties.codec_private_data).toBe('4f707573486561640101000080bb0000000000');
+                }
+                expect(info.tracks[1].properties.codec_private_length).toBe(pcm ? 0 : 19);
+                expect(info.tracks[1].properties.default_track).toBe(true);
+                expect(info.tracks[1].properties.enabled_track).toBe(true);
+                expect(info.tracks[1].properties.forced_track).toBe(false);
+                expect(info.tracks[1].properties.language).toBe('eng');
+                expect(info.tracks[1].properties.minimum_timestamp).toBe(0);
+                expect(info.tracks[1].properties.number).toBe(2);
 
-            ({ stdout, stderr } = await execFileP('ffmpeg', [
-                '-v', 'error',
-                '-i', path,
-                '-f', 'null',
-                '-'
-            ]));
-            expect(stdout).toBe('');
-            expect(stderr).toBe('');
+                ({ stdout, stderr } = await execFileP('ffmpeg', [
+                    '-v', 'error',
+                    '-i', path,
+                    '-f', 'null',
+                    '-'
+                ]));
+                expect(stdout).toBe('');
+                expect(stderr).toBe('');
 
-            // Check there are keyframes in the video
-            ({ stdout, stderr } = await execFileP('ffmpeg', [
-                '-ss', '5',
-                '-noaccurate_seek', // nearest keyframe
-                '-v', 'error',
-                '-i', path,
-                '-f', 'null',
-                '-'
-            ]));
-            expect(stdout).toBe('');
-            expect(stderr).toBe('');
+                // Check there are keyframes in the video
+                ({ stdout, stderr } = await execFileP('ffmpeg', [
+                    '-ss', '5',
+                    '-noaccurate_seek', // nearest keyframe
+                    '-v', 'error',
+                    '-i', path,
+                    '-f', 'null',
+                    '-'
+                ]));
+                expect(stdout).toBe('');
+                expect(stderr).toBe('');
 
-            // Check there are cues in the metadata (i.e. it's seekable).
-            // Exits with status 2 if no cues were found (so throws an exception).
-            ({ stdout, stderr } = await execFileP('mkvextract', [
-                path,
-                'cues', `0:${path}.cues`
-            ]));
-            expect(stdout).toBe(`The cues for track 0 are written to '${path}.cues'.\n`);
-            expect(stderr).toBe('');
-            const cues = (await readFile(`${path}.cues`)).toString();
-            expect(cues.trim().split('\n').length).toBeGreaterThanOrEqual(10);
-        });
+                // Check there are cues in the metadata (i.e. it's seekable).
+                // Exits with status 2 if no cues were found (so throws an exception).
+                ({ stdout, stderr } = await execFileP('mkvextract', [
+                    path,
+                    'cues', `0:${path}.cues`
+                ]));
+                expect(stdout).toBe(`The cues for track 0 are written to '${path}.cues'.\n`);
+                expect(stderr).toBe('');
+                const cues = (await readFile(`${path}.cues`)).toString();
+                expect(cues.trim().split('\n').length).toBeGreaterThanOrEqual(10);
+            });
+        }
     }
 }
